cdf91f67addedca035d3c2d497238efd75c5e4a9
DevFlexDelys <sylvestrefelix89@gmail.com>
2025-08-06 13:51:36 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import './css/Footer.css';
 
 const Footer = () => {
-  return <footer className="footer">&copy; 2054 Antoine Moevus. Tous droits réservés.</footer>;
+  return <footer className="footer">&copy; 2025 Félix Sylvestre-Kentzinger. Tous droits réservés.</footer>;
 }
 
 export default Footer;
