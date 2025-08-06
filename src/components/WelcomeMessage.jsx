b479f4c659e46950d3c4d8535251d6ae504c11cb
DevFlexDelys <sylvestrefelix89@gmail.com>
2025-08-06 13:53:15 -0400

    Update WelcomeMessage.jsx

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 import './css/WelcomeMessage.css';
 
 const WelcomeMessage = () => {
-  return <h1 className="welcome-message">Bienvenue sur notre site web !</h1>;
+  return <h1 className="welcome-message">Bienvenue sur notre site web fictif déployable sur Vercel!</h1>;
 }
 
 export default WelcomeMessage;
