c56e8a23d01c64ded02cc39867a47edfb1936469
DevFlexDelys <sylvestrefelix89@gmail.com>
2025-08-06 13:51:01 -0400

    Update BusinessCard.jsx

--- src/components/BusinessCard.jsx
+++ src/components/BusinessCard.jsx
@@ -3,9 +3,9 @@ import './css/BusinessCard.css';
 const BusinessCard = () => {
   return (
     <div className="business-card">
-      <h2>Jane Doe</h2>
-      <p>Software Engineer</p>
-      <p>jane.doe@example.com</p>
+      <h2>Félix Sylvestre-Kentzinger</h2>
+      <p>Entrepreneur numérique, activiste indépendantiste et étudiant</p>
+      <p>info@kenvertmedia.com</p>
     </div>
   );
 }
